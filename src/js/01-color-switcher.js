329f71f008f3105e650f1976d8199bb954f127fe
Anna0067 <129055045+Anna0067@users.noreply.github.com>
2023-11-28 10:14:22 +0100

    task 1 done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,36 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+let colorChangeInterval;
+
+function startColorChange() {
+  const startButton = document.querySelector('[data-start]');
+  const stopButton = document.querySelector('[data-stop]');
+
+  // Wyłącz przycisk "Start"
+  startButton.disabled = true;
+
+  // Rozpocznij zmianę koloru co sekundę
+  colorChangeInterval = setInterval(() => {
+    document.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function stopColorChange() {
+  const startButton = document.querySelector('[data-start]');
+
+  // Włącz przycisk "Start"
+  startButton.disabled = false;
+
+  // Zatrzymaj zmianę koloru
+  clearInterval(colorChangeInterval);
+}
+
+// Dodaj event listenery do przycisków
+document
+  .querySelector('[data-start]')
+  .addEventListener('click', startColorChange);
+document
+  .querySelector('[data-stop]')
+  .addEventListener('click', stopColorChange);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,41 @@
+import Notiflix from 'notiflix';
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
 }
+
+document
+  .getElementById('promiseForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const formData = new FormData(this);
+    const firstDelay = parseInt(formData.get('delay'));
+    const step = parseInt(formData.get('step'));
+    const amount = parseInt(formData.get('amount'));
+
+    for (let i = 1; i <= amount; i++) {
+      const currentDelay = firstDelay + (i - 1) * step;
+
+      createPromise(i, currentDelay)
+        .then(({ position, delay }) => {
+          notiflix.Notify.Success(
+            `✅ Fulfilled promise ${position} in ${delay}ms`
+          );
+        })
+        .catch(({ position, delay }) => {
+          notiflix.Notify.Failure(
+            `❌ Rejected promise ${position} in ${delay}ms`
+          );
+        });
+    }
+  });
